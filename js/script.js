ec78403f7f6cc2536e30cae5e5d7842ecadf277c
Slawomir Johansen <baran@fastmail.com>
2022-02-09 20:26:35 +0100

    ADD article remove .active

--- js/script.js
+++ js/script.js
@@ -5,22 +5,34 @@
     console.log('links:', links);
   });*/
 
-  const titleClickHandler = function(event){
+const titleClickHandler = function(event){
     console.log(this);
 
   
     /* remove class 'active' from all article links  */
+
+const activeLinks = document.querySelectorAll('.titles a.active');
+
+  for(let activeLink of activeLinks){
+  activeLink.classList.remove('active');
+}
   
     /* add class 'active' to the clicked link */
   
     /* remove class 'active' from all articles */
   
+const activeArticles = document.querySelectorAll('.posts article.active');
+
+  for(let activeArticle of activeArticles){
+  activeArticle.classList.remove('active');
+  }
+
     /* get 'href' attribute from the clicked link */
   
     /* find the correct article using the selector (value of 'href' attribute) */
   
     /* add class 'active' to the correct article */
-  }
+ }
   
   const links = document.querySelectorAll('.titles a');
   
